e3a2a68e815aef779b697ae5b01267f9e9075f70
Braveturtle <guntram.jlng@eduge.ch>
2019-01-31 15:11:23 +0100

    add boulanger.cs add Processing Interface

---
+++ PhotoMaton/IProcessing.cs
@@ -0,0 +1,11 @@
+﻿namespace PhotoMaton
+{
+    interface IProcessing
+    {
+        uint StepCount { get; set; }
+        string Path { get; set; }
+        void DrawStep(int step);
+        void Draw();
+
+    }
+}
