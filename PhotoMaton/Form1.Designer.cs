58af4d9f9f3fa2a8219e373dd9328229be49d844
TuGa_Zz_97 <MICAEL.SNNTSR@eduge.ch>
2019-01-31 15:20:19 +0100

    Add Couplage Class and add 6 methods in Form

--- PhotoMaton/Form1.Designer.cs
+++ PhotoMaton/Form1.Designer.cs
@@ -31,13 +31,21 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fichierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ouvrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.transformationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.photoMatonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.boulangerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.couplageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inverseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.doubleBinaireToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.twistToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fichierToolStripMenuItem});
+            this.fichierToolStripMenuItem,
+            this.transformationToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(928, 24);
@@ -57,6 +65,55 @@
             this.ouvrirToolStripMenuItem.Name = "ouvrirToolStripMenuItem";
             this.ouvrirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.ouvrirToolStripMenuItem.Text = "Ouvrir";
+            // 
+            // transformationToolStripMenuItem
+            // 
+            this.transformationToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.photoMatonToolStripMenuItem,
+            this.boulangerToolStripMenuItem,
+            this.couplageToolStripMenuItem,
+            this.inverseToolStripMenuItem,
+            this.doubleBinaireToolStripMenuItem,
+            this.twistToolStripMenuItem});
+            this.transformationToolStripMenuItem.Name = "transformationToolStripMenuItem";
+            this.transformationToolStripMenuItem.Size = new System.Drawing.Size(100, 20);
+            this.transformationToolStripMenuItem.Text = "Transformation";
+            // 
+            // photoMatonToolStripMenuItem
+            // 
+            this.photoMatonToolStripMenuItem.Name = "photoMatonToolStripMenuItem";
+            this.photoMatonToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.photoMatonToolStripMenuItem.Text = "PhotoMaton";
+            // 
+            // boulangerToolStripMenuItem
+            // 
+            this.boulangerToolStripMenuItem.Name = "boulangerToolStripMenuItem";
+            this.boulangerToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.boulangerToolStripMenuItem.Text = "Boulanger";
+            // 
+            // couplageToolStripMenuItem
+            // 
+            this.couplageToolStripMenuItem.Name = "couplageToolStripMenuItem";
+            this.couplageToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.couplageToolStripMenuItem.Text = "Couplage";
+            // 
+            // inverseToolStripMenuItem
+            // 
+            this.inverseToolStripMenuItem.Name = "inverseToolStripMenuItem";
+            this.inverseToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.inverseToolStripMenuItem.Text = "Inverse";
+            // 
+            // doubleBinaireToolStripMenuItem
+            // 
+            this.doubleBinaireToolStripMenuItem.Name = "doubleBinaireToolStripMenuItem";
+            this.doubleBinaireToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.doubleBinaireToolStripMenuItem.Text = "Double Binaire";
+            // 
+            // twistToolStripMenuItem
+            // 
+            this.twistToolStripMenuItem.Name = "twistToolStripMenuItem";
+            this.twistToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.twistToolStripMenuItem.Text = "Twist";
             // 
             // Form1
             // 
@@ -79,6 +136,13 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fichierToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem ouvrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem transformationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem photoMatonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem boulangerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem couplageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inverseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem doubleBinaireToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem twistToolStripMenuItem;
     }
 }
 
