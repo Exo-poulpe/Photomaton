e5513af5fdb7b9daedad6eb051fa83dd2e52ffb9
TuGa_Zz_97 <MICAEL.SNNTSR@eduge.ch>
2019-01-31 15:42:28 +0100

    Add Element in Couplage class

--- PhotoMaton/Couplage.cs
+++ PhotoMaton/Couplage.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace PhotoMaton
 {
-    class Couplage
+    class Couplage : IProcessing
     {
+        public uint StepCount { get; set; }
+        public string Path { get ; set; }
+
+        public Couplage(string filePath)
+        {
+            this.Path = filePath;
+        }
+
+        public int ppcm(int a, int b)
+        {
+            int p = a * b;
+            while (a != b) if (a < b) b -= a; else a -= b;
+            return p / a;
+        }
+
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawStep(int step)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
