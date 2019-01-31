40a93d5246ae39608888643302872904e5c77d42
TuGa_Zz_97 <MICAEL.SNNTSR@eduge.ch>
2019-01-31 16:06:58 +0100

    Modifiied Couplage Class and add X class

--- PhotoMaton/X.cs
+++ PhotoMaton/X.cs
@@ -7,21 +7,14 @@ using System.Threading.Tasks;
 
 namespace PhotoMaton
 {
-    class Couplage : IProcessing
+    class X : IProcessing
     {
         public uint StepCount { get; set; }
-        public Bitmap Image { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Bitmap Image { get; set; }
 
-        public Couplage(Bitmap imageCouplage)
+        public X(Bitmap imageX)
         {
-            this.Image = imageCouplage;
-        }
-
-        public int ppcm(int a, int b)
-        {
-            int p = a * b;
-            while (a != b) if (a < b) b -= a; else a -= b;
-            return p / a;
+            this.Image = imageX;
         }
 
         public void Draw()
