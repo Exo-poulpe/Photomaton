39b0786eb8f43c9c405c954328547b04318fefd2
Fabian.trllr <33369156+Exo-poulpe@users.noreply.github.com>
2019-01-31 14:52:15 +0100

    First commit

---
+++ PhotoMaton/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace PhotoMaton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.ouvrirToolStripMenuItem.Click += FileInput;
+        }
+
+        public void FileInput(object sender,EventArgs e)
+        {
+            OpenFileDialog opf = new OpenFileDialog();
+            if(opf.ShowDialog() == DialogResult.OK)
+            {
+                string path = opf.FileName;
+                Image img = Image.FromFile(path);
+                this.CreateGraphics().DrawImage(img, new Point(0, 0));
+            }
+
+        }
+
+
+
+    }
+}
