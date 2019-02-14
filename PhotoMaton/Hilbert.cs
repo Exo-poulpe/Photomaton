dfee93fb6cc6ba2949b3a6684ca1a58fbbf75115
Braveturtle <guntram.jlng@eduge.ch>
2019-02-14 14:57:58 +0100

    Add swastika class

---
+++ PhotoMaton/Hilbert.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoMaton
+{
+    class Hilbert : IProcessing
+    {
+        private Bitmap _image;
+        private uint _stepCount;
+
+        private int cpt = 0; private int ind = 0;
+        private int pow;
+        private int dy;
+        private int dx;
+        private int y;
+        private int x;
+        private int ta;
+
+        public uint StepCount { get => _stepCount; set => _stepCount = value; }
+        public Bitmap Image { get => _image; set => _image = value; }
+
+
+
+        public void calcul_matrice()
+        {
+            int size = this.Image.Width;
+            int i = 0;
+            for (;size > 2;i++)
+            {
+                size /= 2;
+            }
+
+
+            forme(1, i);
+
+            addCycle(mc);
+        }
+
+        public void trace(int nx, int ny)
+        {
+            dx = (x + nx * ta);
+            dy = (y + ny * ta);
+            setVal(0, 0, ind, cpt++);
+            mc.addPoint(new Point(dx, dy));
+
+
+            x = dx;
+            y = dy;
+        }
+
+        public void u(int i)
+        {
+            switch (i)
+            {
+                case 1:
+                    trace(0, 1); trace(1, 0); trace(0, -1);
+                    break;
+                case 2:
+                    trace(1, 0); trace(0, 1); trace(-1, 0);
+                    break;
+                case 3:
+                    trace(-1, 0); trace(0, -1); trace(1, 0);
+                    break;
+                case 4:
+                    trace(0, -1); trace(-1, 0); trace(0, 1);
+                    break;
+            }
+
+        }
+
+
+        public void forme(int i, int n)
+        {
+            if (n == 0)
+            {
+                u(i);
+            }
+            else
+            {
+                switch (i)
+                {
+                    case 1:
+                        forme(2, n - 1); trace(0, 1);
+                        forme(1, n - 1); trace(1, 0);
+                        forme(1, n - 1); trace(0, -1);
+                        forme(3, n - 1);
+                        break;
+                    case 2:
+                        forme(1, n - 1); trace(1, 0);
+                        forme(2, n - 1); trace(0, 1);
+                        forme(2, n - 1); trace(-1, 0);
+                        forme(4, n - 1);
+                        break;
+                    case 3:
+                        forme(4, n - 1); trace(-1, 0);
+                        forme(3, n - 1); trace(0, -1);
+                        forme(3, n - 1); trace(1, 0);
+                        forme(1, n - 1);
+                        break;
+                    case 4:
+                        forme(3, n - 1); trace(0, -1);
+                        forme(4, n - 1); trace(-1, 0);
+                        forme(4, n - 1); trace(0, 1);
+                        forme(2, n - 1);
+                        break;
+                }
+            }
+        }
+
+        public Boolean estPossible()
+        {
+            int he = this.Image.Height;
+            int wi = this.Image.Width;
+            he = he <= wi ? he : wi;
+            pow = 1;
+            while (he > 1)
+            {
+                he /= 2;
+                pow <<= 1;
+            }
+
+            return (pow == this.Image.Height) && (pow == wi);
+        }
+        
+        public Bitmap Draw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PhotoMaton/Swastika.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoMaton
+{
+    class Swastika : IProcessing
+    {
+        private uint _stepCount;
+        private Bitmap _image;
+
+        public uint StepCount { get => _stepCount; set => _stepCount = value; }
+        public Bitmap Image { get => _image; set => _image = value; }
+
+        public Swastika(Bitmap pImage)
+        {
+            this.Image = pImage;
+        }
+
+        public Bitmap Draw()
+        {
+            Bitmap tmp = default(Bitmap);
+            tmp = new Bitmap(Image.Width, Image.Height);
+            for (int x = 0; x < Image.Width; x++)
+            {
+                for (int y = 0; y < Image.Height; y++)
+                {
+                    int col = x;
+                    int lig = y;
+                    int l = Image.Width / 2;
+                    int h = Image.Height / 2;
+
+                    if ((x % 2 == 0) && (y % 2 == 0))
+                    {
+                        col = x / 2;
+                        lig = y / 2;
+                    }
+                    else if ((x % 2 == 1) && (y % 2 == 0))
+                    {
+                        col = (x - 1) / 2 + l;
+                        lig = y / 2;
+
+
+                        int milCol = 3 * l / 2;
+                        int milLig = h / 2;
+                        int colR = col - milCol;
+                        int ligR = milLig - lig;
+                        int c = ligR; int li = -colR;
+                        col = milCol + c;
+                        if (Image.Width / 2 % 2 == 0)
+                        {
+                            col--;
+                        }
+                        lig = milLig - li;
+                    }
+                    else if ((x % 2 == 0) && (y % 2 == 1))
+                    {
+                        col = x / 2;
+                        lig = (y - 1) / 2 + h;
+
+
+                        int milCol = l / 2;
+                        int milLig = 3 * h / 2;
+                        int colR = col - milCol;
+                        int ligR = milLig - lig;
+                        int c = -ligR; int li = colR;
+                        col = milCol + c;
+                        lig = milLig - li;
+                        if (Image.Height / 2 % 2 == 0)
+                        {
+                            lig--;
+                        }
+                    }
+                    else
+                    {
+                        col = (x - 1) / 2 + l;
+                        lig = (y - 1) / 2 + h;
+
+                        int milieu = 3 * h / 2;
+                        col += 2 * (milieu - col);
+                        if (Image.Width / 2 % 2 == 0)
+                        {
+                            col--;
+                        }
+                        milieu = 3 * l / 2;
+                        lig += 2 * (milieu - lig);
+                        if (Image.Height / 2 % 2 == 0)
+                        {
+                            lig--;
+                        }
+                    }
+                    tmp.SetPixel(lig, col, Image.GetPixel(x, y));
+                }
+            }
+
+            this.Image = tmp;
+            return this.Image;
+            //return new Point(col, lig);
+        }
+    }
+}
